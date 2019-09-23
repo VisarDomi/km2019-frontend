27190acdaa7dee11a7f65b6f9774e2c245353e7b
Ronald Domi <ronalddomi4@gmail.com>
2019-09-23 19:23:52 +0200

    facebook links

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
 
 module.exports = {
   configureWebpack: {
@@ -22,7 +22,12 @@ module.exports = {
           "/blog9",
           "/blog10",
           "/blog11",
-          "/blog12"
+          "/blog12",
+          "/artist/Lindita/232bd58d-ff5d-41ad-a95b-eac60fdb5cb1",
+          "/artist/Genti%20Deda/264ddaaf-c658-4b12-ae5d-55de99e5c8cc",
+          "/artist/Rea%20Nuhu/5bb5c061-b2b7-4cf5-9f7f-85c8de94d231",
+          "/artist/Khuba/7b7b993b-20bd-4cd4-b17a-d6a4fc6c63d6",
+          "/artist/Sisma/8a2ed33d-38db-4558-a022-a1a120ff5228"
         ]
       })
     ]
