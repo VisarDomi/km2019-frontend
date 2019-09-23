9d6a11bdb21641014198f32bb8fa44b7f980020c
Visar Domi <visardomi4@gmail.com>
2019-09-23 11:33:22 +0200

    test2

--- src/views/VotoArtist.vue
+++ src/views/VotoArtist.vue
@@ -42,9 +42,9 @@
         class="m-2"
         style="text-align: center; font-size: 14px; font-weight: 700;"
       >Sign in or create a new account to vote</h5>
-      <!-- <Loading v-if="getIsLoading" /> -->
       <div id="auth">
-        <amplify-authenticator :authConfig="authConfig"></amplify-authenticator>
+        <!-- <amplify-authenticator :authConfig="authConfig"></amplify-authenticator> -->
+        <AmplifyAuthenticator></AmplifyAuthenticator>
       </div>
     </b-modal>
     <Footer v-if="windowWidth > 770" />
@@ -58,7 +58,8 @@
 import Footer from "@/components/Footer/FooterWhite.vue";
 import FooterSmall from "@/components/Footer/FooterWhiteSmall.vue";
 import FooterMobile from "@/components/Footer/FooterWhiteMobile.vue";
-// import Loading from "@/components/Loading.vue";
+
+import AmplifyAuthenticator from "@/components/AwsCustomComponent.vue";
 
 import { mapGetters } from "vuex";
 import { GET_ARTIST, PUT_VOTES } from "@/store/actions.type";
@@ -77,7 +78,7 @@ export default {
     Footer,
     FooterSmall,
     FooterMobile,
-    // Loading
+    AmplifyAuthenticator
   },
   data() {
     return {
@@ -139,10 +140,7 @@ export default {
         accessToken
       };
       console.log("put send");
-      this.$store.commit(START_LOADING);
       await this.$store.dispatch(PUT_VOTES, params);
-      this.$store.commit(STOP_LOADING);
-
       this.disabled = true;
       // if (this.getVote === "Voted.") {
       // }
