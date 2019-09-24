8c631fa56d604e8daf353976a274418b08b6f90a
Visar Domi <visardomi4@gmail.com>
2019-09-24 19:20:03 +0200

    footers mobile

--- src/components/Footer/FooterSingleBlogMobile.vue
+++ src/components/Footer/FooterSingleBlogMobile.vue
@@ -8,20 +8,25 @@
 
         <div class="col-7">
           <div class="row links-row ml-0">
-            <div class="col-4">
-              <div class="row" style="margin-bottom:5px;" @click="changeRoute('Home')">artistët</div>
+            <a class="col-4">
+              <a class="row" style="margin-bottom:5px;" @click="changeRoute('Home')" v-if="this.lang == 'en'">artists</a>
+              <a class="row" style="margin-bottom:5px;" @click="changeRoute('Home')" v-else>artistët</a>
 
-              <div class="row" @click="changeRoute('Home')">ndër vite</div>
+              <a class="row" href="/#nder-vite-mobile" v-if="this.lang == 'en'">throgh the years</a>
+              <a class="row" href="/#nder-vite-mobile" v-else>ndër vite</a>
+            </a>
+
+            <div class="col-4">
+              <div class="row" style="margin-bottom:5px;" @click="changeRoute('Home')" v-if="this.lang == 'en'">#magjike</div>
+              <div class="row" style="margin-bottom:5px;" @click="changeRoute('Home')" v-else>#magjike</div>
+
+              <div class="row" href="/#te-reja-mobile" v-if="this.lang == 'en'">të reja</div>
+              <div class="row" href="/#te-reja-mobile" v-else>të reja</div>
             </div>
 
             <div class="col-4">
-              <div class="row" style="margin-bottom:5px;" @click="changeRoute('Home')">#magjike</div>
-
-              <div class="row" @click="changeRoute('Home')">të reja</div>
-            </div>
-
-            <div class="col-4">
-              <div class="row" style="margin-bottom:5px;" @click="changeRoute('Home')">rregullore</div>
+              <div class="row" style="margin-bottom:5px;" @click="changeRoute('Rules')" v-if="this.lang == 'en'">rules</div>
+              <div class="row" style="margin-bottom:5px;" @click="changeRoute('Rules')" v-else>rregullore</div>
 
               <div class="row" @click="changeRoute('Home')">voto</div>
             </div>
@@ -69,11 +74,20 @@
 </template>
 
 <script>
+import { getLanguage, saveLanguage } from "@/store/services/storage";
 export default {
   methods: {
     changeRoute(name) {
       this.$router.push({ name: name });
     }
+  },
+  data() {
+    return {
+      lang: ""
+    };
+  },
+  mounted(){
+    this.lang = getlanguage();
   }
 };
 </script>
