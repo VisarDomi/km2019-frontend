f1dbb24e10f9c9d25bf5411df85d35410815903c
ErdalDomi <erdal.domi@gmail.com>
2019-09-25 11:31:42 +0200

    ip console log with npm innstall

--- src/main.js
+++ src/main.js
@@ -30,6 +30,46 @@ Amplify.configure(awsmobile);
 // Amplify.configure(config)
 Vue.use(AmplifyPlugin, AmplifyModules);
 
+
+// var IPGeolocationAPI = require('ip-geolocation-api-javascript-sdk');
+
+// // Create IPGeolocationAPI object. Constructor takes two parameters.
+// // 1) API key (Optional: To authenticate your requests through "Request Origin", you can skip it.)
+// // 2) Async (Optional: It is used to toggle "async" mode in the requests. By default, it is true.)
+// var ipgeolocationApi = new IPGeolocationAPI("1f0bd03d73e247a8b5a77bb64ba53268")
+
+// // Function to handle response from IP Geolocation API
+// function handleResponse(json) {
+//   console.log(json);
+// }
+
+// var GeolocationParams = require('ip-geolocation-api-javascript-sdk/GeolocationParams.js');
+
+// // Get complete geolocation for the calling machine's IP address
+// ipgeolocationApi.getGeolocation(handleResponse);
+
+// An example script for redirecting users from USA to https://google.com/
+// and users from Canada to https://google.ca/
+
+// ip-api endpoint URL
+// we need only the countryCode, but you can request more fields
+// see http://ip-api.com/docs/api:json for documentation
+var endpoint = 'http://ip-api.com/json/?fields=query';
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+    var response = JSON.parse(this.responseText);
+    console.log("response is:" , response)
+		if(response.status !== 'success') {
+			console.log('query failed: ' + response.message);
+			return
+		}
+	}
+};
+xhr.open('GET', endpoint, true);
+xhr.send();
+
 Vue.use(vueScrollTo);
 Vue.config.productionTip = false;
 Vue.use(VuePageTransition);
