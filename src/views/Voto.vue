5393d20246e5516367a96346355139ff5f4c91f7
ErdalDomi <erdal.domi@gmail.com>
2019-09-30 15:43:23 +0200

    voting list new artist update

--- src/views/Voto.vue
+++ src/views/Voto.vue
@@ -113,21 +113,44 @@ export default {
         this.artists.push({});
       }
       for (let artist of this.getArtists) {
-        if (artist.name == "Lindita") {
-          this.artists[0] = artist;
+if (artist.name == "Lindita") {
+          this.artists[6] = artist;
           // continue
         }
         if (artist.name == "Genti Deda") {
-          this.artists[1] = artist;
+          this.artists[7] = artist;
         }
         if (artist.name == "Rea Nuhu") {
-          this.artists[2] = artist;
+          this.artists[8] = artist;
         }
         if (artist.name == "Khuba") {
-          this.artists[3] = artist;
+          this.artists[9] = artist;
         }
         if (artist.name == "Sisma") {
+          this.artists[10] = artist;
+        }
+
+        if (artist.name == "Erik Lloshi") {
+          this.artists[0] = artist;
+        }
+        if (artist.name == "Korab Shaqiri") {
+          this.artists[1] = artist;
+        }
+        if (artist.name == "Elia") {
+          this.artists[2] = artist;
+          // this.artists2.push(artist);
+        }
+        if (artist.name == "Alex Alexander") {
+          this.artists[3] = artist;
+          // this.artists2.push(artist);
+        }
+        if (artist.name == "Laura&Elisa Gjipi") {
           this.artists[4] = artist;
+          // this.artists3.push(artist);
+        }
+        if (artist.name == "Kristi") {
+          this.artists[5] = artist;
+          // this.artists3.push(artist);
         }
       }
     },
